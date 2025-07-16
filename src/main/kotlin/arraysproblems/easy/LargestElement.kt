93fd2097ca6378311d0715f72e7fa1bc3b93ce1f
sparrow007 <ankit.steven007@gmail.com>
2025-07-16 06:49:11 +0530

    adding the array easy questions

---
+++ src/main/kotlin/arraysproblems/easy/LargestElement.kt
@@ -0,0 +1,90 @@
+package arraysproblems.easy
+
+import java.util.SortedSet
+
+fun main() {
+    for (i in 9 downTo 0) {
+        print(i)
+    }
+    val rArray = intArrayOf(10)
+    rArray.set()
+    rArray[0] = 90
+
+}
+
+fun maxValue() {
+    val array  = Array<Int>(5) { i -> i }
+
+    var max = -1
+    var secondMax = -1
+
+
+    for (i in 0 until 20)
+
+    for (i in array) {
+        if (i > max) {
+            max = i
+        }
+        if (i in (secondMax + 1) until max) {
+            secondMax = i
+        }
+    }
+
+    println(secondMax)
+}
+
+fun checkIfArraySorted() {
+    val array = intArrayOf()
+    var isArraySorted = false
+
+    if (array.size == 1) isArraySorted = true
+
+    //check for ascending/
+    for (i in 0..array.size) {
+
+        if ((i+1) < array.size) {
+            isArraySorted =  array[i] <= array[i+1]
+            if (!isArraySorted) break
+        }
+    }
+
+    print("is array sorted $isArraySorted")
+}
+
+fun removeDuplicates(nums: IntArray): Int {
+
+    val hashMap= LinkedHashSet<Int>()
+
+    for (i in nums) {
+        hashMap.add(i)
+    }
+
+    println(hashMap.toSortedSet())
+
+    hashMap.forEachIndexed { index: Int, i: Int ->
+        nums[index] = i
+    }
+
+    return hashMap.size
+
+}
+
+
+fun moveZeroes(nums: IntArray): Unit {
+    val size = nums.size
+    for (i in 0 until size) {
+        if (nums[i] == 0) {
+            var j = i
+            while (j < size && nums[j] != 0) {
+                j++
+            }
+
+            if(j >= size) break
+
+            val temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = i
+        }
+    }
+
+}
